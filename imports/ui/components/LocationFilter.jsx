5ebb134bde26236fcc3df411d8620df3e2a107c0
tatomyr <andrew.tatomyr@gmail.com>
2016-12-13 17:23:30 +0200

    Show districts depending on region

--- imports/ui/components/LocationFilter.jsx
+++ imports/ui/components/LocationFilter.jsx
@@ -7,18 +7,49 @@ class LocationFilter extends React.Component {
   constructor(props) {
     super(props);
 
+    this.state = {
+      'statistics.administrative_area_level_1': null // this.refs.selectAdmAreaLev1.value,
+    };
   }
 
+  selectAdmAreaLev1() {
+    console.log('-->', this.refs.selectAdmAreaLev1.value);
+    this.setState({
+      'statistics.administrative_area_level_1': this.refs.selectAdmAreaLev1.value // this.refs.selectAdmAreaLev1.value,
+    });
+  }
 
+  renderAdmAreaLev1() {
+    return (<select ref="selectAdmAreaLev1" onChange={this.selectAdmAreaLev1.bind(this)}>
+      <option key='All Ukraine'>All Ukraine</option>
+      {this.props.localities.
+        filter(item => item.type === 'administrative_area_level_1' /* && item.parentId === null */).
+        map(item => (<option key={item.placeId} value={item.placeId}>{item.name}</option>))}
+    </select>);
+  }
+
+  selectAdmAreaLev2() {
+
+    // return ();
+  }
+
+  renderAdmAreaLev2() {
+    return (<select onChange={this.selectAdmAreaLev2.bind(this)}>
+      <option key='Whole region' value='Whole region'>Whole region</option>
+      {this.props.localities.
+        filter(item => item.type === 'administrative_area_level_2' && item.parentId === this.state['statistics.administrative_area_level_1']).
+        map(item => (<option key={item.placeId} value={item.placeId}>{item.name}</option>))}
+    </select>);
+  }
 
   render() {
     console.log(this.props.localities);
     return (
       <div className="LocationFilter-wrapper">
         <h3>Select area:</h3>
-        <select>
-          <option>All Ukraine</option>
-        </select>
+        {this.renderAdmAreaLev1()}
+
+        {this.renderAdmAreaLev2()}
       </div>
     )
   }

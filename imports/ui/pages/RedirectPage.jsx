c769ebfa550f1c39211c0eb478115fa024061399
TarasPyzo <taraspyzo@gmail.com>
2017-01-19 12:25:43 +0200

    redirect to email

--- imports/ui/pages/RedirectPage.jsx
+++ imports/ui/pages/RedirectPage.jsx
@@ -10,7 +10,7 @@ export default class LoginPage extends React.Component {
 
   render() {
     return (
-      <h2> This Page gonna redirect you somewhere </h2>
+      <h2> Please go to your email service and check email </h2>
     );
   }
 

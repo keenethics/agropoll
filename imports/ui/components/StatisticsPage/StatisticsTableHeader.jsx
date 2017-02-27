63d27b9e12e38b0f39b9bb614f32efd94aab98d3
tatomyr <andrew.tatomyr@gmail.com>
2017-02-27 14:23:04 +0200

    added comparising graph for squares

--- imports/ui/components/StatisticsPage/StatisticsTableHeader.jsx
+++ imports/ui/components/StatisticsPage/StatisticsTableHeader.jsx
@@ -4,6 +4,7 @@ export default () => (
   <div className="row table-header">
     <div className="cell coll-1">Культура</div>
     <div className="cell coll-2">Площа вирощування у [базовий] рік</div>
+    <div className="cell coll-3">Базова до прогнозованої</div>
     <div className="cell coll-3">Прогнозована площа вирощування</div>
   </div>
 );
--- imports/ui/components/StatisticsPage/StatisticsTableRow.jsx
+++ imports/ui/components/StatisticsPage/StatisticsTableRow.jsx
@@ -11,6 +11,19 @@ export default class StatisticsTableRow extends React.Component {
           <span>{this.props.crop.squares[this.props.regionId].toFixed(2)}</span>
         </div>
         <div className="cell coll-3">
+          <div
+            className="square-forecast"
+            title={(this.props.cropsView.totalSquare / 1000).toFixed(2)}
+          >
+            <div
+              className="square-base"
+              style={{ width: `${100 * this.props.crop.squares[this.props.regionId] / (this.props.crop.squares[this.props.regionId] + (this.props.cropsView.totalSquare / 1000))}%` }}
+              title={this.props.crop.squares[this.props.regionId].toFixed(2)}
+            >
+            </div>
+          </div>
+        </div>
+        <div className="cell coll-4">
           <span>{(this.props.cropsView.totalSquare / 1000).toFixed(2)}</span>
           {/* <span>{this.props.cropsView.harvest.toFixed(0)}</span> */}
         </div>

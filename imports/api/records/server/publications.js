5097302da77bcc92bc48eb20d2f7668f468f1c62
tatomyr <andrew.tatomyr@gmail.com>
2017-03-02 19:00:45 +0200

    added admin table

--- imports/api/records/server/publications.js
+++ imports/api/records/server/publications.js
@@ -2,8 +2,10 @@
 
 import { Meteor } from 'meteor/meteor';
 import { check, Match } from 'meteor/check';
+import { Roles } from 'meteor/alanning:roles';
 
 import { Records } from '../records.js';
+
 
 Meteor.publish('records.filter', function(filters) {
   check(filters, {
@@ -60,27 +62,49 @@ Meteor.publish('records.user', function () {
     throw new Meteor.Error('not-authorized');
   }
 
-  return Records.find({ userId: this.userId }, {
-    fields: {
-      // userId: 0,
-      // squareNorm: 0,
-      // farmlandArea: 0,
-    },
-  });
+  return Records.find({ userId: this.userId });
 });
 
 // Only for Admin
-Meteor.publish('records.all', function () {
-  // IF ADMIN
-  if (false) {
-    throw new Meteor.Error('not-authorized');
+Meteor.publish('records.filter.admin', function (filters) {
+  check(filters, {
+    planned: Boolean,
+    planted: Boolean,
+    harvested: Boolean,
+    place_id: Match.OneOf(String, null),
+    administrative_area_level_1: Match.OneOf(String, null),
+    administrative_area_level_2: Match.OneOf(String, null),
+    year: String,
+  });
+
+  if (!Roles.userIsInRole(this.userId, 'admin')) {
+    console.log(`! WARNING ! \nUser '${this.userId}' was trying to access to admin page!`);
+    // throw new Meteor.Error('User is not Admin!');
   }
 
-  return Records.find({}, {
-    fields: {
-      // userId: 0,
-      // squareNorm: 0,
-      // farmlandArea: 0,
-    },
-  });
+  const statuses = {
+    planned: filters.planned,
+    planted: filters.planted,
+    harvested: filters.harvested,
+  };
+
+  const locationFilter = filters.place_id || filters.administrative_area_level_2 ||
+  filters.administrative_area_level_1;
+
+  const query = {
+    year: filters.year,
+    status: { $in: Object.keys(statuses).filter((item) => statuses[item]) },
+  };
+
+  if (locationFilter !== 'none') {
+    query.$or = [
+      { 'location.administrative_area_level_1': locationFilter },
+      { 'location.administrative_area_level_2': locationFilter },
+      { 'location.place_id': locationFilter },
+    ];
+  }
+
+  console.log('admin query =', query);
+
+  return Records.find(query);
 });
--- imports/ui/pages/AdminPage.jsx
+++ imports/ui/pages/AdminPage.jsx
@@ -3,24 +3,58 @@ import React from 'react';
 import { createContainer } from 'meteor/react-meteor-data';
 
 // import { Crops, Groups } from '/imports/api/crops/crops.js';
-// import { Records } from '/imports/api/records/records.js';
-// import { Users } from '/imports/api/users/users.js';
+import { Records } from '/imports/api/records/records.js';
+
 
 // import StatisticsTableRow from '/imports/ui/components/StatisticsPage/StatisticsTableRow.jsx';
 // import StatisticsTableHeader from '/imports/ui/components/StatisticsPage/StatisticsTableHeader.jsx';
-// import LocationFilter from '/imports/ui/components/StatisticsPage/LocationFilter.jsx';
-// import StatusFilter from '/imports/ui/components/StatisticsPage/StatusFilter.jsx';
-// import YearSelector from '/imports/ui/components/YearSelector.jsx';
+import LocationFilter from '/imports/ui/components/StatisticsPage/LocationFilter.jsx';
+import StatusFilter from '/imports/ui/components/StatisticsPage/StatusFilter.jsx';
+import YearSelector from '/imports/ui/components/YearSelector.jsx';
 
-// import { connect } from 'react-redux';
+import Griddle from 'griddle-react';
+
+import { connect } from 'react-redux';
 
 class AdminPage extends React.Component {
+  griddleData() {
+    console.log(Records.findOne());
+
+    return this.props.users.length && this.props.users.map((user) => ({
+      User: user.emails[0].address,
+      'Total square': this.props.records.filter((item) => item.userId === user._id).reduce((prev, next) => prev + +next.square, 0),
+      Locations: user.profile.locations,
+      Type: user.profile.type,
+      Cluster: '',
+      Role: '',
+    })) || [{
+      User: '',
+      'Total square': '',
+      Locations: '',
+      Type: '',
+      Cluster: '',
+      Role: '',
+    }];
+  }
 
   render() {
-    console.log(this.props.users);
-
     return (
       <div>
+        <div className="filter-bar">
+          <div className="statistic-one">
+            <YearSelector />
+          </div>
+          <div className="statistic-two">
+            <StatusFilter />
+          </div>
+          <div className="statistic-three">
+            <LocationFilter />
+          </div>
+        </div>
+
+        <div className="statistic-content">
+          <Griddle data={this.griddleData()} />
+        </div>
 
       </div>
     );
@@ -29,23 +63,23 @@ class AdminPage extends React.Component {
 
 const container = createContainer((props) => {
   const user = Meteor.user();
-  Meteor.subscribe('users.all');
+  const usersHandler = Meteor.subscribe('users.all');
   // Meteor.subscribe('crops.all');
   // Meteor.subscribe('groups.all');
-  // const recordsHandler = Meteor.subscribe('records.all', { ...props.statisticsTable, ...props.all });
+  const recordsHandler = Meteor.subscribe('records.filter.admin', { ...props.statisticsTable, ...props.all });
   // console.info('Records ready:', recordsHandler.ready());
   // const records = recordsHandler.ready() ? Records.find({}).fetch() : [];
+  const users = usersHandler.ready() ? Meteor.users.find({}).fetch() : [];
   return {
     user,
-    users: Meteor.users.find({}).fetch(),
+    users,
     // crops: Crops.find({}, { sort: { id: 1 } }).fetch(),
     // groups: Groups.find({}, { sort: { id: 1 } }).fetch(),
-    // records,
+    records: Records.find({}).fetch(),
   };
 }, AdminPage);
 
-export default container;
+// export default container;
 
-// const mapStateToProps = (state) => ({ statisticsTable: state.statisticsTable, all: state.all });
-//
-// export default connect(mapStateToProps)(container);
+const mapStateToProps = (state) => ({ statisticsTable: state.statisticsTable, all: state.all });
+export default connect(mapStateToProps)(container);

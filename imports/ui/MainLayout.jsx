62a7efd93977c4ad9552b0fc092eba57bf692335
tatomyr <andrew.tatomyr@gmail.com>
2017-03-13 01:46:04 +0200

    added footer

--- imports/ui/MainLayout.jsx
+++ imports/ui/MainLayout.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import NavigationBar from '/imports/ui/components/NavigationBar/NavigationBar.jsx';
 import AppContainer from '/imports/ui/containers/AppContainer.jsx';
+import Footer from '/imports/ui/components/Footer.jsx';
 
 export default class MainLayout extends React.Component {
 
@@ -11,6 +12,7 @@ export default class MainLayout extends React.Component {
         <AppContainer>
           {this.props.children}
         </AppContainer>
+        <Footer />
       </div>
     );
   }
---
+++ imports/ui/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default () => (
+  <div className="footer">
+    <div className="footer-item">
+      <a href="https://keenethics.com" target="blank">Keenethics © 2017</a>
+    </div>
+  </div>
+);

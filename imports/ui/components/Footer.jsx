9264c4c4f79783ec610ba5aa5e80b6738b0d0ac6
tatomyr <andrew.tatomyr@gmail.com>
2017-03-21 20:05:14 +0200

    footer start change lang

--- imports/ui/components/Footer.jsx
+++ imports/ui/components/Footer.jsx
@@ -1,16 +1,40 @@
 import React from 'react';
 
-export default () => (
-  <div className="footer">
-    <div className="footer-item">
-      developed by&nbsp;
-      <a
-        href="https://keenethics.com"
-        target="blank"
-        className="footer-link"
-      >
-        keenethics
-      </a>
-    </div>
-  </div>
-);
+
+export default class Footer extends React.Component {
+
+  changeLanguage(language) {
+    localStorage.setItem('language', language);
+    console.log('---->', localStorage.getItem('language'));
+  }
+
+
+  render() {
+    return (
+      <div className="footer">
+        other languages:
+        <button
+          onClick={() => this.changeLanguage('ua')}
+        >
+          Українська
+        </button>
+        <button
+          onClick={() => this.changeLanguage('en')}
+        >
+          English
+        </button>
+
+        <div className="footer-item">
+          developed by&nbsp;
+          <a
+            href="https://keenethics.com"
+            target="blank"
+            className="footer-link"
+          >
+            keenethics
+          </a>
+        </div>
+      </div>
+    );
+  }
+}
